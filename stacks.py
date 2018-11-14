e8eed98c5bb22cd7e32fd989438fef2ad218fcf8
helenzhao093 <helen.zhao1026@gmail.com>
2018-11-14 14:01:09 -0500

    add port

--- stacks.py
+++ stacks.py
@@ -302,3 +302,6 @@ def convert_csv_to_array(csv_filename, convert_to_int, quoting):
 @app.route('/static/<path:path>')
 def send_js(path):
     return send_from_directory('static', path)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
