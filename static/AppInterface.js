12035554997bfa08e595d53e03fc3d25957725f4
Helen Zhao <h.zhao@salesforce.com>
2019-07-13 22:51:30 -0700

    more typos

--- static/AppInterface.js
+++ static/AppInterface.js
@@ -345,7 +345,7 @@ class AppInterface extends React.Component {
 
             // send prior to graph and initialize graph
             this.sendData("/initializeGraph", {
-                userId: userID,
+                userID: userID,
                 forbiddenEdges: forbiddenEdgesInfo.forbiddenEdges,
                 requiredEdges: requiredEdgesInfo.requiredEdges
             });
