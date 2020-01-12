3fc02b3d64fe6e93ca97d6a25387670d076300ab
Helen Zhao <h.zhao@salesforce.com>
2020-01-12 12:42:27 -0800

    fix fit

--- ifs/Classifier.py
+++ ifs/Classifier.py
@@ -67,13 +67,13 @@ class Classifier:
         skf = StratifiedKFold(n_splits=5)
         for train_index, test_index in skf.split(X_traintest, y_traintest):
             train_x, test = X_traintest[train_index], X_traintest[test_index]
-            train_y, test_y = y[train_index], y[test_index]
+            train_y, test_y = y_traintest[train_index], y_traintest[test_index]
             self.clf.fit(train_x, train_y)
             accuracy_train.append(self.clf.score(X_train, y_train))
             accuracy_test.append(self.clf.score(X_test, y_test)) # testing accuracy
             accuracy_traintest.append(self.clf.score(X_traintest, y_traintest))
             accuracy_validation.append(self.clf.score(X_validation, y_validation))# validation accuracy
-            
+
         predicted = self.clf.predict(X_train)
         self.proba = self.clf.predict_proba(X_train)
 
