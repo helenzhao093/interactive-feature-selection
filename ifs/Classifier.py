2b7746843e9048b6994da6fcdc1204e967f68dbb
Helen Zhao <h.zhao@salesforce.com>
2020-01-21 22:54:24 -0800

    use gaussian

--- ifs/Classifier.py
+++ ifs/Classifier.py
@@ -23,7 +23,7 @@ class Classifier:
         df_test_og = pd.read_csv(dirname + 'test_datafile.csv')
         df_validate_og = pd.read_csv(dirname + 'validation_datafile.csv')
 
-        self.clf = RandomForestClassifier(max_depth=2, random_state=0) #GaussianNB()
+        self.clf = GaussianNB()
         self.clf_roc = OneVsRestClassifier(GaussianNB())
         self.accuracy = 0
         self.precision = 0
